87249724eeb0b80a4d2189a5fc04f869065240a2
user3 <user3@qq.com>
2019-04-13 09:16:14 +0800

    xxxxxxxxxxxxxxxxxx

--- src/cn/itsource/GitTtest.java
+++ src/cn/itsource/GitTtest.java
@@ -3,5 +3,6 @@ package cn.itsource;
 public class GitTtest {
     public static void main(String[] args) {
         System.out.println("0000000000011111111111110000");
+        System.out.println("00000000000");
     }
 }
